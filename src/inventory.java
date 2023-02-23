82446267bbb0a0ba3f09ca2a1549e96c24e093d8
aum9518 <abduganiuulumanasbek@gmail.com>
2023-02-23 14:59:19 +0600

    add search, typeSearch, discount method

--- src/inventory.java
+++ src/inventory.java
@@ -22,7 +22,7 @@ public class inventory implements Searchable{
     }
 
     @Override
-    public Product searchbyType(String type ) {
+    public Product searchbyType(String type ) {  //tibine jazsak oshol tiptegi productunu chygaryp beret
         for (Product b: product) {
             if (b.getType().name().equals(type)){
                 return b;
@@ -38,9 +38,9 @@ public class inventory implements Searchable{
           //  Period prp =Period.ofDays(10);
             int prp1 = Period.between(c.getMadeOfDate(), LocalDate.now()).getDays();
             if (prp1<10){
-            c.setStatusExpirationDate("Valid");
+            c.setStatusExpirationDate("Valid");//eger 10 kumdon ahkan emes bolso statusexpiration poliasyna jaraktuu maanisin beret
             }else {
-                c.setStatusExpirationDate("Invalid");
+                c.setStatusExpirationDate("Invalid"); //eger 10 kundon ashyp ketse jaraksyz maaanisin beret
             }
         }
     }
@@ -51,9 +51,9 @@ public class inventory implements Searchable{
             Period month = Period.ofMonths(6);
             int month1 = Period.between(s.getMadeOfDate(),LocalDate.now()).getMonths();
             if (month1>6){
-                s.setStatusExpirationDate("Discount 50%");
+                s.setStatusExpirationDate("Discount 50%"); //eger productuga 6 aidan ashsa anda 50% skidka maanisin beret
             }else {
-               s.setStatusExpirationDate("New one");
+               s.setStatusExpirationDate("New one"); //eger 6 ai bolo elek bolso jany tovar dep chygat
             }
         }
     }
